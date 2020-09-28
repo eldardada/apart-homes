b4fce442de73868e8b9e7e7ff564a8bbb4f8eb0d
Эльдар Дадашов <eldar-dadashov@mail.ru>
2020-09-28 11:34:59 +0300

    Сверстан блок offers и queries

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,26 @@
-if(document.querySelector('.swiper-container')) {
+if(document.querySelector('.swiper-wrapper')) {
 	@@include('libs/swiper.min.js');
+}
+
+if(document.querySelector('.offers-slider')) {
+	let offersSlider = new Swiper('.offers-slider', {
+		loop: true,
+		slidesPerView: 3,
+		slidesPerGroup: 3,
+		spaceBetween: 30,
+		navigation: {
+			prevEl: '.offers__prev',
+			nextEl: '.offers__next'
+		}
+	});
+}
+if(document.querySelector('.image-slider')) {
+	let imageSlider = new Swiper('.image-slider', {
+		loop: true,
+		lazy: true,
+		navigation: {
+			prevEl: '.image-slider__prev',
+			nextEl: '.image-slider__next'
+		}
+	});
 }
